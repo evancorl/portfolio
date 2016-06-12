872cb067a7ed027ce034f3ce938d7cec253abb39
Evan Corl <evancorl@yahoo.com>
2016-06-11 23:17:07 -0500

    Update mobile config to be dark theme

--- mobile-config.js
+++ mobile-config.js
@@ -1,11 +1,11 @@
-App.setPreference('BackgroundColor', '#fff');
+App.setPreference('BackgroundColor', '#313131');
 
 App.setPreference('DisallowOverscroll', 'true');
 App.setPreference('webviewbounce', 'false');
 
 App.setPreference('StatusBarOverlaysWebView', 'false');
-App.setPreference('StatusBarBackgroundColor', '#fff');
-// App.setPreference('StatusBarStyle', 'lightcontent');
+App.setPreference('StatusBarBackgroundColor', '#313131');
+App.setPreference('StatusBarStyle', 'lightcontent');
 
 App.setPreference('FadeSplashScreen', 'false');
 App.setPreference('ShowSplashScreenSpinner', 'false');
