b825f212f0ef44f6c7a97795525cf2cc19319b97
Evan Corl <evancorl@Evans-MacBook-Air.local>
2016-06-26 16:48:58 -0500

    Create post body mockup

--- imports/client/components/post/post-body.js
+++ imports/client/components/post/post-body.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 class PostBody extends React.Component {
   shouldComponentUpdate() {
@@ -6,9 +7,24 @@ class PostBody extends React.Component {
   }
 
   render() {
+    const title = 'INTRODUCING CHRIS COLBOURN - Presented By Element';
+
+    const message = 'Chris Colbourn has been a Berrics favorite since we first saw him skate a '
+      + 'couple years ago. His talent and creativity on a skateboard are unreal. We\'re very '
+      + 'proud to present his brand new street part, presented by Element. This is just the tip '
+      + 'of the iceberg...';
+
     return (
-      <div className="post-body mobile-container">
-        <img className="post-image" src="/images/feeds.jpg" />
+      <div className="post-body">
+        <div className="post-media mobile-container">
+          <img className="post-image" src="/images/feeds.jpg" />
+        </div>
+        <div className="container">
+          <div className="post-title">
+            <Link to="" className="active-hover">{title}</Link>
+          </div>
+          <p className="post-message">{message}</p>
+        </div>
       </div>
     );
   }
