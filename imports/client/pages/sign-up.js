326af2a497f80225299d9bbd26cb63b722b96d19
Evan Corl <evancorl@Evans-MacBook-Air.local>
2016-06-26 14:34:16 -0500

    Create temporary signup page

---
+++ imports/client/pages/sign-up.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import FacebookSignup from '../components/signup/facebook-signup';
+
+const SignupPage = () => (
+  <div className="col-left">
+    <div className="content-box container inner">
+      <FacebookSignup />
+    </div>
+  </div>
+);
+
+export default SignupPage;
