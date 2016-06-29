8110fd67154b2b03dc0e54337556d29decdbae0b
Evan Corl <evancorl@Evans-MacBook-Air.local>
2016-06-28 22:34:30 -0500

    Write placeholder HTML for post actions component

--- imports/client/components/post/post-actions.js
+++ imports/client/components/post/post-actions.js
@@ -7,7 +7,25 @@ class PostActions extends React.Component {
 
   render() {
     return (
-      <div className="post-actions">
+      <div className="inner-x">
+        <div className="post-actions-box">
+          <div className="post-stats">
+            <button type="button" className="post-stat active-hover">
+              16 likes
+            </button>
+            <button type="button" className="post-stat active-hover">
+              5 comments
+            </button>
+          </div>
+          <div className="post-actions">
+            <button type="button" className="post-action icon icon-heart">
+              Like
+            </button>
+            <button type="button" className="post-action icon icon-comment">
+              Comment
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
