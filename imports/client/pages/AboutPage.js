3f281dd15ccc870f3bab2cfdf36b1704b2bb3bb9
Evan Corl <evancorl@yahoo.com>
2016-07-24 00:52:00 -0500

    Render AboutBand on AboutPage

--- imports/client/pages/AboutPage.js
+++ imports/client/pages/AboutPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import IntroBand from '../components/IntroBand';
+import AboutBand from '../components/AboutBand';
 
 const AboutPage = () => (
   <div className="about-page">
     <IntroBand />
+    <AboutBand />
   </div>
 );
 
