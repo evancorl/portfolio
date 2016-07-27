f0bf42c3f2198a13809f27080123873198c1e44c
Evan Corl <evancorl@yahoo.com>
2016-07-27 00:02:37 -0500

    Create FooterConnectList component

---
+++ imports/client/components/MainLayout/Footer/FooterConnectList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const FooterConnectList = ({ list }) => (
+  <ul className="footer-connect-list">
+    {Object.keys(list).map((service, i) => (
+      <li key={i} className={`footer-connect-item icon icon-${service.toLowerCase()}`}>
+        <a href={list[service]} target="_blank">{service}</a>
+      </li>
+    ))}
+  </ul>
+);
+
+FooterConnectList.propTypes = {
+  list: React.PropTypes.object.isRequired,
+};
+
+export default FooterConnectList;
