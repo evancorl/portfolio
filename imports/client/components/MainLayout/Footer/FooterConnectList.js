203a30f8632cbefe49009a62c2ae5eef78c2845e
Evan Corl <evancorl@yahoo.com>
2016-07-27 00:35:33 -0500

    Open footer email link in same window

--- imports/client/components/MainLayout/Footer/FooterConnectList.js
+++ imports/client/components/MainLayout/Footer/FooterConnectList.js
@@ -4,7 +4,7 @@ const FooterConnectList = ({ list }) => (
   <ul className="footer-connect-list">
     {Object.keys(list).map((service, i) => (
       <li key={i} className={`footer-connect-item icon icon-${service.toLowerCase()}`}>
-        <a href={list[service]} target="_blank">{service}</a>
+        <a href={list[service]} target={service === 'Email' ? '_self' : '_blank'}>{service}</a>
       </li>
     ))}
   </ul>
