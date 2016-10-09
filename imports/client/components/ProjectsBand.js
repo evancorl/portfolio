0d1660e80d3b02997c627314dc8eaafe73d8fbdc
Evan Corl <evancorl@yahoo.com>
2016-10-09 01:19:56 -0500

    Create projects band component

---
+++ imports/client/components/ProjectsBand.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import ProjectListContainer from '../containers/ProjectListContainer';
+
+class ProjectsBand extends React.Component {
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  render() {
+    return (
+      <div className="projects-band inner-hor inner-ver">
+        <div className="col-wide">
+          <h1 className="projects-title">Projects</h1>
+          <ProjectListContainer />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProjectsBand;
