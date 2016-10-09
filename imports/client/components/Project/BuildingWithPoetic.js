90fd7772e056daf2663b6a96d5beb47bfd3dece0
Evan Corl <evancorl@yahoo.com>
2016-10-09 00:33:28 -0500

    Create Building with Poetic component

---
+++ imports/client/components/Project/BuildingWithPoetic.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const BuildingWithPoetic = () => (
+  <span>
+    <span>Building with </span>
+    <span className="icon icon-poetic"></span>
+  </span>
+);
+
+export default BuildingWithPoetic;
