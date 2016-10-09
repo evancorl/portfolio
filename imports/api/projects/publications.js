7444a8b0985d986a90cbc1bd247eb29da0b067b5
Evan Corl <evancorl@yahoo.com>
2016-10-09 00:32:31 -0500

    Add hardcoded data for project list information

---
+++ imports/api/projects/publications.js
@@ -0,0 +1,35 @@
+const projects = [
+  {
+    name: 'YouTrade',
+    image: '/images/projects/youtrade.png',
+    description: 'A real-time hybrid app that sends stock market tips to paying subscribers.',
+    languages: ['HTML', 'CSS', 'JavaScript (ES6)'],
+    tools: ['Meteor', 'React', 'Mongo', 'Sass', 'Atmosphere', 'NPM'],
+  },
+  {
+    name: 'Camden Living',
+    image: '/images/projects/camden-living.png',
+    description: 'A Houston-based real estate investment company that owns and operates over 150 properties.',
+    languages: ['HTML', 'CSS', 'JavaScript (ES6)', 'PHP'],
+    tools: ['Drupal', 'Express', 'Meteor', 'React', 'Mongo', 'Google Maps', 'NPM'],
+    url: 'http://www.camdenliving.com/',
+  },
+  {
+    name: 'City Choice Homes',
+    image: '/images/projects/city-choice-homes.png',
+    description: 'An urban living builder that lets aspiring homeowners search by neighborhood and community.',
+    languages: ['HTML', 'CSS', 'PHP', 'JavaScript', 'SQL', 'XML'],
+    tools: ['Drupal', 'MySQL', 'Google Maps'],
+    url: 'http://www.citychoicehomes.com/',
+  },
+  {
+    name: 'Hijinx Life',
+    image: '/images/projects/hijinx-life.png',
+    description: 'What started as a college project turned into an MVP for a local business.',
+    languages: ['HTML', 'CSS', 'JavaScript', 'C#', 'SQL'],
+    tools: ['ASP.NET', 'Entity Framework', 'Microsoft SQL Server'],
+    url: 'http://www.hijinxlife.com/home/',
+  },
+];
+
+export default projects;
