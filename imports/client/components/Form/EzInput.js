31907551491d79a37b1014883eeab6a0783e55bb
Evan Corl <evancorl@yahoo.com>
2016-10-09 20:39:07 -0500

    Create EzInput component

---
+++ imports/client/components/Form/EzInput.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+class EzInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.renderErrorMessage = this.renderErrorMessage.bind(this);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.errorMessage !== nextProps.errorMessage;
+  }
+
+  renderErrorMessage(placement) {
+    const { errorMessage, errorBeforeInput } = this.props;
+
+    const shouldRender = errorMessage && 
+      (placement === 'BEFORE' && errorBeforeInput || placement === 'AFTER' && !errorBeforeInput);
+
+    return shouldRender
+      ? <span className="ez-error-msg">{errorMessage}</span>
+      : null;
+  }
+
+  render() {
+    const {
+      children,
+      className,
+      errorMessage,
+    } = this.props;
+
+    let inputClass = 'ez-input';
+
+    if (className) inputClass += ' ' + className;
+    if (errorMessage) inputClass += ' ez-error';
+
+    return (
+      <div className={inputClass}>
+        {this.renderErrorMessage('BEFORE')}
+        {children}
+        {this.renderErrorMessage('AFTER')}
+      </div>
+    );
+  }
+}
+
+EzInput.propTypes = {
+  errorMessage: React.PropTypes.string,
+  errorBeforeInput: React.PropTypes.bool,
+};
+
+EzInput.defaultProps = {
+  errorMessage: null,
+  errorBeforeInput: false,
+};
+
+export default EzInput;
