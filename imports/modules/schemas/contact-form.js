b225446a9285903fa77d707d2efa871f0b9fb63d
Evan Corl <evancorl@yahoo.com>
2016-10-09 20:45:31 -0500

    Create folders and file for contact form schema

---
+++ imports/modules/schemas/contact-form.js
@@ -0,0 +1,19 @@
+const contactFormSchema = {
+  name: {
+    messages: {
+      required: 'Please enter your name',
+    },
+  },
+  emailAddress: {
+    messages: {
+      required: 'Please enter your email address',
+    },
+  },
+  message: {
+    messages: {
+      required: 'Please don\'t give me the silent treatment :\\',
+    },
+  }
+};
+
+export default contactFormSchema;
